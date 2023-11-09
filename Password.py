044595ea944e9271342a2601eada286c6d8d0368
IsaacMoAl <145053507+IsaacMoAl@users.noreply.github.com>
2023-11-09 13:09:31 -0600

    Password.py

---
+++ Password.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_code():
+  length = 8
+  password =''.join(random.choices('0123456789', k=length))
+  return password 
+
+n_password = generate_code()
+
+print("My new password is", n_password)
